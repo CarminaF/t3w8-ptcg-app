58d86376e7c0aac326cd636804a1fa1494b9f88c
Carmina Familar <99389599+CarminaF@users.noreply.github.com>
2023-10-07 12:15:43 +1030

    css added to cards, added navbar (#17)
    * card search functionality
    
    * card search functionality with image
    
    * remove warnings
    
    * remove warnings
    
    * css added to cards, added navbar

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import ApiProvider from './contexts/ApiContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CardSearchByName from './pages/CardSearchByName';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavSearchBar from './components/NavSerachBar';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ApiProvider>
       <BrowserRouter>
+      <NavSearchBar />
         <Routes>
           {/* Homepage route */}
           <Route path="/" element={<App />}/>
